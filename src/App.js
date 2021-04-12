7b30fd1fbab1f8058015add40bd4970245319134
danielohayon636 <72417937+danielohayon636@users.noreply.github.com>
2021-04-12 12:41:25 +0300

    login&register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
+import SignInSide from "./components/Login/SignInSide";
+
 import RecipeReviewCard from "./components/RecipeReviewCard";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
@@ -20,31 +22,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="main">
-        <Grid
-          container
-          spacing={4}
-          className={classes.gridContainer}
-          justify="center"
-        >
-          <Grid item xs={12} sm={6} md={4}>
-            <RecipeReviewCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <RecipeReviewCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <RecipeReviewCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <RecipeReviewCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <RecipeReviewCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <RecipeReviewCard />
-          </Grid>
-        </Grid>
+        <SignInSide />
       </div>
       <Footer />
     </div>
