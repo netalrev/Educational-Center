dca3cec5803d83fb646f94a584244fda2e6a6eac
danielohayon636 <72417937+danielohayon636@users.noreply.github.com>
2021-04-12 22:37:09 +0300

    addded

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import './Button.css'
+import React from "react";
+import "./Button.css";
+import SignUp from "./Register/SignUp";
 
-const STYLES = [
-    'btn--primary',
-    'btn--outline'
-]
-const SIZES = [
-    'btn--medium',
-    'btn--large'
-]
+import { useHistory } from "react-router-dom";
+const STYLES = ["btn--primary", "btn--outline"];
+const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
-    children,
-    type,
-    onClick,
-    buttonStyle,
-    buttonSize
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
-    return (
-        <button className = {`btn ${checkButtonStyle} ${checkButtonSize}`} onClick = {onClick} type = {type}>
-        {children}
-        </button>
-    )
-}
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  const history = useHistory();
+
+  const routeChange = () => {
+    let path = `register`;
+    history.push(path);
+  };
+
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={routeChange}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
