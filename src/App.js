9079084ea3e21f3b0114b59f99bedd39645bd699
danielohayon636 <72417937+danielohayon636@users.noreply.github.com>
2021-04-18 14:31:40 +0300

    AWS Amplify added.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
 import SignInSide from "./components/Login/SignInSide";
-import SignUp from "./components/Register/SignUp";
+//import AmplifySignUp from "./components/Register/AmplifySignUp"
 import RecipeReviewCard from "./components/RecipeReviewCard";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,9 +13,27 @@ import {
   Route,
   useParams,
 } from "react-router-dom";
-import ContactForm from './components/ContactUs/ContactForm';
-import ActivitiesPage from './components/Activities/ActivitiesPage';
-import ClassesPage from './components/Classes/ClassesPage';
+import ContactForm from "./components/ContactUs/ContactForm";
+import ActivitiesPage from "./components/Activities/ActivitiesPage";
+import ClassesPage from "./components/Classes/ClassesPage";
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import {
+  AmplifyAuthenticator,
+  AmplifySignUp,
+  AmplifySignIn,
+  AmplifySignOut,
+} from "@aws-amplify/ui-react";
+import { I18n } from "aws-amplify";
+
+Amplify.configure(awsconfig); //AWS CONFIGORE
+
+var name;
+Auth.currentAuthenticatedUser().then(
+  (user) =>
+    //alert(user.attributes.given_name)
+    (name = user.attributes.given_name)
+);
 
 const useStyles = makeStyles({
   gridContainer: {
@@ -23,6 +41,28 @@ const useStyles = makeStyles({
     paddingRight: "10px",
   },
 });
+
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Email",
+      key: "username",
+      required: true,
+      placeholder: "Email",
+      type: "email",
+      displayOrder: 1,
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      placeholder: "Password",
+      type: "password",
+      displayOrder: 2,
+    },
+  ],
+};
 
 function App() {
   const classes = useStyles();
@@ -34,22 +74,71 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/">
-              <RecipeReviewCard />
+              <h1>{name}</h1>
             </Route>
             <Route exact path="/register">
-              <SignUp />
+              <AmplifyAuthenticator>
+                <AmplifySignUp
+                  slot="sign-up"
+                  usernameAlias="email"
+                  headerText="הרשמה"
+                  formFields={[
+                    {
+                      type: "email",
+                      label: "Custom email Label",
+                      placeholder: "custom email placeholder",
+                      required: true,
+                    },
+                    {
+                      type: "password",
+                      label: "Custom Password Label",
+                      placeholder: "custom password placeholder",
+                      required: true,
+                    },
+                    {
+                      label: "Confirm Password",
+                      name: "password2",
+                      required: true,
+                      type: "password",
+                    },
+
+                    {
+                      type: "phone_number",
+                      label: "Custom Phone Label",
+                      placeholder: "custom Phone placeholder",
+                      required: true,
+                    },
+                    {
+                      type: "given_name",
+                      label: "שם פרטי",
+                      placeholder: "שם פרטי",
+                      required: true,
+                    },
+                    {
+                      type: "family_name",
+                      label: "שם משפחה",
+                      placeholder: "שם משפחה",
+                      required: true,
+                    },
+                  ]}
+                />
+                <AmplifySignIn slot="sign-in" usernameAlias="email" />
+              </AmplifyAuthenticator>
             </Route>
             <Route exact path="/login">
-              <SignInSide />
+              <AmplifySignIn />
+            </Route>
+            <Route exact path="/SignOut">
+              <AmplifySignOut />
             </Route>
             <Route exact path="/contactus">
-              <ContactForm /> 
+              <ContactForm />
             </Route>
             <Route exact path="/activitiespage">
-              <ActivitiesPage /> 
+              <ActivitiesPage />
             </Route>
             <Route exact path="/classespage">
-              <ClassesPage /> 
+              <ClassesPage />
             </Route>
           </Switch>
         </Router>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
 import "./Navbar.css";
 import { Button } from "../Button";
+import { Auth } from "aws-amplify";
+
+var name;
+Auth.currentAuthenticatedUser().then(
+  (user) =>
+    //alert(user.attributes.given_name)
+    (name = user.attributes.given_name)
+);
 
 class Navbar extends Component {
   state = { clicked: false }; //for the menu btn
@@ -12,14 +20,15 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="NavbarItems">
-        <h1 className="navbar-logo">
-          <i
-            className="fab fa-react"
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href = "/"; //Url to icon button.
-            }}
-          ></i>
+        <h1
+          className="navbar-logo"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "/"; //Url to icon button.
+          }}
+        >
+          <i className="fab fa-react"></i>
+          המרחב החינוכי השלם - {name}
         </h1>
 
         <div className="menu-icon" onClick={this.handleClick}>
