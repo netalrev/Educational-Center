102050345146989bd659eecd90f2d3bab2b808ad
Netanel <netanelreuven1@gmail.com>
2021-05-20 14:34:31 +0300

    improve validation

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import "./profile.css";
 import Amplify, { Auth } from "aws-amplify";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
+
+var level = 1;
+var nextLevel = 100;
+var registeredActivities = 1;
+var pastActivities = 1;
+var procces = registeredActivities / registeredActivities;
+
 class profile extends Component {
   render() {
     return (
@@ -17,36 +24,37 @@ class profile extends Component {
           <a href="https://codepen.io/AlbertFeynman/" target="_blank">
             {this.props.givenName + " " + this.props.familyName}
           </a>
-          <h6 title="Followers">
-            <i className="fas fa-users"></i>{" "}
-            <span className="followers">90</span>
+          <h6 title="Level">
+            <i className="fas fa-user-graduate"></i>{" "}
+            <span className="Level">{" "} {level} {" "} </span>
           </h6>
         </div>
 
         <div className="ds-info">
           <div className="ds pens">
             <h6 title="Number of pens created by the user">
-              פעילויות <i className="fas fa-edit"></i>
+              רישום לפעילויות <i className="fas fa-edit"></i>
             </h6>
-            <p>29</p>
+            <p>{" "} {registeredActivities} {" "}</p>
           </div>
           <div className="ds projects">
             <h6 title="Number of projects created by the user">
-              שיעורים <i className="fas fa-project-diagram"></i>
+              התקדמות בפעילויות <i className="fas fa-project-diagram"></i>
             </h6>
-            <p>4</p>
+            <p>{" "} {procces} {" "}</p>
           </div>
           <div className="ds posts">
             <h6 title="Number of posts">
-              משימות להגשה <i className="fas fa-comments"></i>
+              דרגה הבאה <i className="fas fa-comments"></i>
             </h6>
-            <p>0</p>
+            <p>{" "} {nextLevel} {" "}</p>
           </div>
         </div>
         <div className="ds-skill">
           <h6>
             Skill <i className="fa fa-code" aria-hidden="true"></i>
           </h6>
+
           <div className="skill html">
             <h6>
               <i className="fab fa-html5"></i> HTML5{" "}
@@ -63,7 +71,6 @@ class profile extends Component {
               <p>90%</p>
             </div>
           </div>
-
           <div className="skill javascript">
             <h6>
               <i className="fab fa-js"></i> JavaScript{" "}
