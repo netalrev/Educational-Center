da4f49e4b2d14d64e507149083065a5ff95b0a4a
danielohayon636 <72417937+danielohayon636@users.noreply.github.com>
2021-06-07 19:33:49 +0300

    s

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
 ReactDOM.render(<App />, document.getElementById("root"));
-//Or you can do it where ever you want, base on user selected language, or something else
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
