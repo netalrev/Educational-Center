34a6845fd4da7879969799d8e7b93a12ca89da98
Netanel <netanelreuven1@gmail.com>
2021-06-10 14:36:35 +0300

    aa

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -123,7 +123,7 @@ export default function SignIn() {
     e.preventDefault();
   }
 
-  //HTML code with styling for spesific tags.
+  //REACT Component with styling for spesific tags.
   return (
     <Container component="main" maxWidth="xs" id="allForm">
       <CssBaseline />
